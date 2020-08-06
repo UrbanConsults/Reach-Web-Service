34f9d926918eac4800a5d1f70c93a6ef6467db32
Scott Lott <Scott@ClickSimply.com>
2020-08-06 12:05:52 -0700

    api updates

--- example-app/bin/index.ts
+++ example-app/bin/index.ts
@@ -1,6 +1,6 @@
 
 export default [
-    (rws: any, db: any, fs: any, mail: any) => { // install
+    (rws: any, db: any, fs: any, mail: any, elasticSearch: any) => { // install
         
         rws.addCronJob("* * * * *", async () => {
 
@@ -33,83 +33,98 @@ export default [
         }, 10);
 
         // only works with DB native module
-        db.migrate(1, async (migrate) => {
+        db.migrate(1 /* current migration version */, async (migrate) => {
 
-            // make previous version available
+            // make previous version of migration available
             // migrate.old_version = 0
             
-            migrate.newComp("login", {
-                type: "table",
-                columns: [
-                    ["username", {type: "string"}],
-                    ["email", {type: "string"}],
-                    ["phone", {type: "string"}]
-                ]
-            }, {
+            await migrate.newComp({
+                name: "login",
+                secure: false,
+                model: {
+                    type: "table",
+                    columns: [
+                        ["username", {type: "string"}],
+                        ["email", {type: "string"}],
+                        ["phone", {type: "string"}]
+                    ]
+                }
+            });
+
+            // user id index
+            await migrate.newIndex({
+                extend: "users",
+                name: "login-index",
+                watch: "login", // component to watch?
+                data_type: "string(32)", // string(25), decimal(2), geo(4), uint8, etc
+                value: (insert, loginComponent) => {
+                    [
+                        loginComponent.username,
+                        loginComponent.email,
+                        loginComponent.phone
+                    ].filter(v => v && v.length).forEach(v => {
+                        insert(String(v).toLowerCase());
+                    });
+                }
+            });
+
+            await migrate.newType({
+                name: "order-totals-by-day",
+                keyModel: {type: "u64"},
+                defaultComps: ["order-count"]
+            });
+
+            await migrate.newComp({
+                name: "order-count",
+                model: {type: "u64"},
                 secure: false
             });
 
-            /*
-
-            migrate.newIndex("user-id", {
-                type: "string[]", // string, geo, uint8, uint8[], etc
-                watch: "login", // which component to watch for this index?
-                value: (loginComponent) => {
-                    return [
-                        loginComponent.username,
-                        loginComponent.email,
-                        loginComponent.phone
-                    ].filter(v => v && v.length).map(v => String(v).toLowerCase())
-                }
-            });
-            */
-
-            // user id index
-            migrate.newView("login-index", {
-                watch: "login", // watch component?
-                key: "string",
-                type: "ulid",
-                prev: async (view, entityType, entityID, oldLoginComponent) => { // get old (previous records)
-                    let oldKeys = [
-                        oldLoginComponent.username,
-                        oldLoginComponent.email,
-                        oldLoginComponent.phone
-                    ].filter(v => v && v.length).map(v => [String(v).toLowerCase()]);
-
-                    return await view.find(["IN", oldKeys]);
-                },
-                update: async (view, oldKVs, entityType, entityID, oldLoginComponent, loginComponent) => { // update them
-                    let newKeys = [
-                        loginComponent.username,
-                        loginComponent.email,
-                        loginComponent.phone
-                    ].filter(v => v && v.length).map(v => [String(v).toLowerCase()]);
-
-                    return await view.update(newKeys.map(login_str => ({
-                        k: login_str,
-                        v: entityID
-                    })));
+            await migrate.newIndex({
+                extend: "order-totals-by-day",
+                name: "order-count-index",
+                watch: "order-count",
+                data_type: "u64",
+                value: (index, orderCountComp) => {
+                    index.add(orderCountComp);
                 }
             });
 
-            // aggregate post count for users
-            migrate.newView("post-count", {
-                watch: "forum-post", // watch component?
-                key: "ulid",
-                type: "u32",
-                prev: async (view, entityType, entityID, oldForumPost) => { // get old (previous records)
-                    if (!oldForumPost.author) return false; // return false to stop execution
-                    // return [] to just jump to update KV
-                    return await view.find(["=", oldForumPost.author]);
-                },
-                update: async (view, oldKVs, entityType, entityID, oldForumPost, newForumPost) => { // update them
-                    const useKV = oldKVs && oldKVs.length ? oldKVs[0] : {k: newForumPost.author, v: 0};
+            await migrate.newTrigger({
+                name: "user-search-index",
+                watch: "users.user-data",
+                call: async (userData) => {
+                    await elasticSearch.update(userData);
+                }
+            })
 
-                    if (!oldForumPost) { // only increment if it's a new forum post
-                        useKV.v++;
+            await migrate.newAggregate({
+                name: "calculate-daily-orders", 
+                watch: "order.order-data", // watch what type & component?
+                apply: "order-totals-by-day.order-count", // which type are we modifying?
+                key: (orderData) => {
+                    return orderData.day;
+                },
+                value: (isNewRecord, orderData, oldOrderCount) => {
+                    if (!oldOrderCount) {
+                        oldOrderCount = 0;
                     }
+                    if (isNewRecord) {
+                        oldOrderCount++;
+                    } else {
+                        return false; // don't update anything
+                    }
+                    return oldOrderCount;
+                }
+            });
 
-                    return await view.update([useKV]);
+            await migrate.newIndex({
+                extend: "blogs",
+                name: "authors",
+                watch: "blog-post-data",
+                data_type: "ulid",
+                value: (index, blogPostData) => {
+                    index.add(blogPostData.authorID);
                 }
             });
 
@@ -117,9 +132,19 @@ export default [
             // compIndex.entityType
             // *.compIndex should work to get any entity type for a given index
             // let users = await db.select(["id", "or", "other", "components"]).fromIndex("user-id.users").find(["=", "login@gmail.com"], {limit: 1});
-            let users = await db.select(["id", "or", "other", "components"]).fromType("users").all({limit: 1});
-            users = await db.select(["id", "or", "other", "components"]).fromType("users").find(["=", "entity ID"], {limit: 1});
-            users = await db.select(["id", "or", "other", "components"]).fromType("users").find(["BETWEEN", ["lower ID", "higher ID"]], {limit: 1});
+            let users = await db.select(["id", "or", "other", "components"]).from("users").all({limit: 1});
+            users = await db.select(["id", "or", "other", "components"]).from("users").find(["=", "entity ID"], {limit: 1});
+            users = await db.select(["id", "or", "other", "components"]).from("users").find(["BETWEEN", ["lower ID", "higher ID"]], {limit: 1});
+            // use index
+            users = await db.select(["id", "or"]).from("users.login-index").find(["=", "user@gmail.com"], {limit: 1});
+            
+            // graph API
+            users = await db.select(["id", "or", "other", {
+                key: "blog-posts",
+                query: ["id", async (id_comp) => {
+                    return await db.select(["blog-post-data"]).from("blogs.authors").find(["=", id_comp], {limit: 5})
+                }]
+            }]).from("users").find(["BETWEEN", ["lower ID", "higher ID"]], {limit: 1});
             
             /*
             users = await db.select(["id", "or", "other", "components"]).fromAggr("post-count.users").find(["BETWEEN", ["lower ID", "higher ID"]], {limit: 1});
@@ -135,58 +160,39 @@ export default [
                 query: await db.select(["blog-data"]).fromRel("blog-author.blog-posts").find(["=", id], {limit: 5})
             })]).fromType("users").all({limit: 1});
             */
-            migrate.newComp("password", {
-                type: "string"
-            }, {
+           await migrate.newComp({
+                name: "password",
+                model: {type: "string"},
                 secure: true
             });
 
-
-
-            migrate.newComp("forum-user-data", {
-                type: "string"
-            }, {
+            await migrate.newComp({
+                name: "forum-user-data",
+                model: {type: "table", columns: [
+                    ["favorite-post", {type: "ulid"}]
+                ]},
                 secure: true, // can the client side see this?
-                depends: ["login", "user-account"],
-                indexes: {
-                    
-                }
+                depends: ["login", "user-account"]
             });
 
-            /*
-            // instead of group by
-            migrate.newAggr("post-count", {
-                type: "uint32"
-            }, {
-                watch: "forum-posts", // which component to watch for this aggregate?
-                getId: (record) => {
-                    return record.userId;
-                },
-                value: (oldEntity, newEntity, aggregateValue) => {
-                    if (!aggregateValue) {
-                        aggregateValue = 0;
-                    }
-                    if (!oldEntity) { // no old entity, new post
-                        aggregateValue++;
-                    }
-                    return aggregateValue;
-                },
-                sortKey: (aggregateValue) => { // optional if you need sorting for this aggregate
-                    return aggregateValue;
-                }
+            await migrate.newType({
+                name: "user",
+                keyModel: {type: "ulid"},
+                defaultComps: [
+                    "user-account",
+                    "login",
+                    "password"
+                ]
             });
-            */
+
+            await migrate.newType({
+                name: "blog-post",
+                keyModel: {type: "ulid"},
+                defaultComps: [
+                    "blog-post-data"
+                ]
+            });
             
-            migrate.newType("user", "uuid" /* or ulid */, [
-                // default entity components
-                "user-account",
-                "login",
-                "password"
-            ]);
-
-            migrate.newType("blog-post", "ulid", [
-                "blog-post-data"
-            ]);
             /*
             migrate.newRelation("blog-author", "blog-post[] <=> user");  // bidrectional, many to one, can query both
             migrate.newRelation("blog-author", "blog-post[] => user");  // one direction, many to one, can query against blog-posts
